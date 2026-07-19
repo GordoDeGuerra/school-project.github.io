0d05d15b6f7de815a9c383a3f2b7b3369becfa9c
GordoDeGuerra <j.cruz19@escola.pr.gov.br>
2026-07-19 19:52:39 -0300

    Add event listeners for multiple buttons
    Updated button event listeners to include multiple buttons.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function eventobotao() {
-const botao = document.getElementById("botao1");
 console.log("botão clicado");
 alert("você clicou no botão");
 }
-const botao = document.getElementById("botao1");
-botao.addEventListener('click', eventobotao);
+const botao1 = document.getElementById("botao1");
+botao1.addEventListener('click', eventobotao);
+const botao2 = document.getElementById("botao2");
+botao2.addEventListener('click', eventobotao);
+const botao3 = document.getElementById("botao3");
+botao3.addEventListener('click', eventobotao);
   
